bce01dd7865a47ba3d917c6dacf197fa735eb7aa
Andrey Antukh <niwi@niwi.be>
2015-02-19 00:51:20 +0100

    Add .gitignore and project.clj

---
+++ project.clj
@@ -0,0 +1,8 @@
+(defproject bytebuf "0.1.0-SNAPSHOT"
+  :description "FIXME: write description"
+  :url "http://example.com/FIXME"
+  :license {:name "Eclipse Public License"
+            :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :dependencies [[org.clojure/clojure "1.7.0-alpha5"]
+                 [io.netty/netty-buffer "4.1.0.Beta3"]
+                 [potemkin "0.3.11"]])
