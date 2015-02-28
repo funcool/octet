8b11064158f38281031a3de15bfd7a4b393111aa
Andrey Antukh <niwi@niwi.be>
2015-02-28 13:43:18 +0100

    Remove run.js script.

--- test/run.js
+++
@@ -1,9 +0,0 @@
-try {
-    require("source-map-support").install();
-} catch(err) {
-}
-require("../output/out/goog/bootstrap/nodejs.js");
-require("../output/tests.js");
-
-goog.require("bytebuf.runner");
-goog.require("cljs.nodejscli");
