ea09706b189f1e10dae358cd9ce9722efde4a1a3
Andrey Antukh <niwi@niwi.nz>
2015-11-03 18:23:31 +0300

    Add build scripts.

---
+++ scripts/build.clj
@@ -0,0 +1,14 @@
+(require '[cljs.build.api :as b])
+
+(println "Building ...")
+
+(let [start (System/nanoTime)]
+  (b/build
+   (b/inputs "test" "src")
+   {:main 'octet.tests.core
+    :output-to "out/tests.js"
+    :output-dir "out"
+    :optimizations :advanced
+    :target :nodejs
+    :verbose true})
+  (println "... done. Elapsed" (/ (- (System/nanoTime) start) 1e9) "seconds"))
