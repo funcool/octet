b2295388cdd949f71a2e0076267f452efbfa4af8
Andrey Antukh <niwi@niwi.nz>
2018-01-15 13:44:59 +0100

    Update dependencies.

--- project.clj
+++ project.clj
@@ -3,8 +3,8 @@
   :url "https://github.com/funcool/octet"
   :license {:name "Public Domain"
             :url "http://unlicense.org/"}
-  :dependencies [[org.clojure/clojure "1.8.0" :scope "provided"]
-                 [org.clojure/clojurescript "1.9.293" :scope "provided"]
+  :dependencies [[org.clojure/clojure "1.9.0" :scope "provided"]
+                 [org.clojure/clojurescript "1.9.946" :scope "provided"]
                  [io.netty/netty-buffer "4.1.5.Final"]]
 
   :source-paths ["src"]
@@ -21,7 +21,13 @@
             :src-uri "http://github.com/funcool/codeina/blob/master/"
             :src-uri-prefix "#L"}
 
-  :plugins [[funcool/codeina "0.5.0"]
-            [lein-ancient "0.6.10"]])
+  :profiles
+  {:dev {:dependencies [[org.clojure/tools.namespace "0.2.11"]]
+         :aliases {"test-all" ["with-profile" "dev,1.8:dev,1.7:dev" "test"]}
+         :global-vars {*warn-on-reflection* false}
+         :plugins [[funcool/codeina "0.5.0"]
+                   [lein-ancient "0.6.15"]]}
+   :1.8 {:dependencies [[org.clojure/clojure "1.8.0"]]}
+   :1.7 {:dependencies [[org.clojure/clojure "1.7.0"]]}})
 
 
